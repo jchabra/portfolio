5cd374b032c69dfd7293f9fa07c72e7ad297caf4
Jasmine <jchabra@gmail.com>
2015-12-10 12:03:40 -0500

    initial commit

---
+++ javascripts/contact.js
@@ -0,0 +1,44 @@
+//Form Validation
+$('#contact_form').validate({
+  rules: {
+    name: 'required',
+    email: {
+      required: true,
+      email: true
+    },
+    message: 'required',
+    human: {
+      required: true,
+      digits: true
+    }
+  },
+  messages: {
+    name: 'Required',
+    email: 'A valid email is required',
+    message: 'Please include a message',
+    human: 'Do the math before you submit your message!'
+  },
+  errorPlacement: function(error, element) {
+    error.insertBefore( element );
+  },
+  submitHandler: function(form) {
+    $(form).ajaxSubmit({
+      type:"POST",
+      data: $(form).serialize(),
+      url:"index.php",
+      success: function() {
+        $('#contact_form :input').attr('disabled', 'disabled');
+        $('#contact_form').fadeTo( "slow", 0.15, function() {
+          $(this).find(':input').attr('disabled', 'disabled');
+          $(this).find('label').css('cursor','default');
+          $('#success').fadeIn();
+        });
+      },
+      error: function() {
+        $('#contact_form').fadeTo( "slow", 0.15, function() {
+          $('#error').fadeIn();
+        });
+      }
+    });
+  }
+});
---
+++ javascripts/nav.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  $('nav a').click(scroll);
+
+  $(window).scroll(showReturn);
+  $(window).resize(showReturn);
+  $('#returntotop').click(returnTop);
+
+});
+
+function scroll()
+{
+  $.scrollTo( this.hash, 900, { easing: 'easeOutQuad', axis: 'y' });
+  return false;
+  // need return false so that the href normally there doesn't fire off
+}
+
+function returnTop()
+{
+  $.scrollTo( "#wrapper", 900, { easing: 'easeOutQuad', axis: 'y' });
+}
+
+function showReturn()
+{
+  if (($(this).scrollTop() > 300))
+    {
+        $('#returntotop').fadeIn();
+    }
+  else
+    {
+        $('#returntotop').fadeOut();
+    }
+}
+
+
+
