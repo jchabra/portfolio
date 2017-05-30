9867dc92f7155183cffaf52f1ca16de897763a25
Jasmine <jchabra@gmail.com>
2017-05-30 14:47:00 -0400

    updated the style of the skills section

--- javascripts/skills.js
+++ javascripts/skills.js
@@ -1,31 +1,31 @@
 // Web Development
-var webDev=new Array("HTML5", "CSS3", "SASS", "Responsive Design", "JavaScript", "jQuery", "WCAG 2.0", "Foundation", "Bootstrap", "Unsemantic");
+var webDev=new Array("HTML5,&nbsp;", "CSS3,&nbsp;", "SASS,&nbsp;", "Responsive Design,&nbsp;", "JavaScript,&nbsp;", "jQuery,&nbsp;", "WCAG 2.0,&nbsp;", "508 Guidelines,&nbsp;","Foundation,&nbsp;", "Bootstrap,&nbsp;", "Unsemantic");
 
 for(var i=0; i<webDev.length; i++)
-    $('#skills').find("div.webDevSkills").append('<div class="skillContainer grid-33 mobile-grid-50"><div class="skillBox">' + webDev[i] + '</div></div>');
+    $('#skills').find("div.webDevSkills").append('<div class="skillContainer"><div class="skillName">' + webDev[i] + '</div></div>');
 
 // Design
-var design=new Array("Axure", "Sketch", "InVision", "Photoshop", "Illustrator", "InDesign",  "Adobe XD", "Keynote", "Unity 3D");
+var design=new Array("Axure,&nbsp;", "Sketch,&nbsp;", "InVision,&nbsp;", "Photoshop,&nbsp;", "Illustrator,&nbsp;", "InDesign,&nbsp;",  "Adobe XD,&nbsp;", "Keynote,&nbsp;", "Unity 3D");
 
 for(var i=0; i<design.length; i++)
-    $('#skills').find("div.designSkills").append('<div class="skillContainer grid-33 mobile-grid-50"><div class="skillBox">' + design[i] + '</div></div>');
+    $('#skills').find("div.designSkills").append('<div class="skillContainer"><div class="skillName">' + design[i] + '</div></div>');
 
 // Management
-var management=new Array("Github", "FogBugz", "Trello", "Pivotal Tracker", "Cyberduck", "MS Office");
+var management=new Array("Github,&nbsp;", "FogBugz,&nbsp;", "Trello,&nbsp;", "Pivotal Tracker,&nbsp;", "Cyberduck,&nbsp;", "MS Office");
 
 for(var i=0; i<management.length; i++)
-    $('#skills').find("div.managementSkills").append('<div class="skillContainer grid-33 mobile-grid-50"><div class="skillBox">' + management[i] + '</div></div>');
+    $('#skills').find("div.managementSkills").append('<div class="skillContainer"><div class="skillName">' + management[i] + '</div></div>');
 
 // Database
-var database=new Array("PostgreSQL", "Oracle SQL");
+var database=new Array("PostgreSQL,&nbsp;", "Oracle SQL");
 
 for(var i=0; i<database.length; i++)
-    $('#skills').find("div.databaseSkills").append('<div class="skillContainer grid-33 mobile-grid-50"><div class="skillBox">' + database[i] + '</div></div>');
+    $('#skills').find("div.databaseSkills").append('<div class="skillContainer"><div class="skillName">' + database[i] + '</div></div>');
 
 // Cloud Applications
-var cloud=new Array("Heroku", "Amazon S3", "Google Cloud");
+var cloud=new Array("Heroku,&nbsp;", "Amazon S3,&nbsp;", "Google Cloud");
 
 for(var i=0; i<cloud.length; i++)
-    $('#skills').find("div.cloudSkills").append('<div class="skillContainer grid-33 mobile-grid-50"><div class="skillBox">' + cloud[i] + '</div></div>');
+    $('#skills').find("div.cloudSkills").append('<div class="skillContainer"><div class="skillName">' + cloud[i] + '</div></div>');
 
 
